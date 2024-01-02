9f3f3346e0620bffcdf0c4eee9b8db03e9d01b54
Senait7-ux <119904111+Senait7-ux@users.noreply.github.com>
2024-01-02 13:19:49 -0500

    * UI update * Query Update for globla status bar

--- src/bento/globalStatsData.js
+++ src/bento/globalStatsData.js
@@ -1,15 +1,15 @@
 import gql from 'graphql-tag';
 import studiesLogo from '../assets/stats/Studies_Logo.svg';
 import participantsLogo from '../assets/stats/Participants_Logo.svg';
-import samplesLogo from '../assets/stats/Samples_Logo.svg';
-import fileLogo from '../assets/stats/Files_Logo.svg';
+import diagnosisLogo from '../assets/stats/Diagnosis_Logo.svg';
+
 
 export const statsStyling = {
   global: {
     horizontalStyle: false,
     statTitleFirst: false,
     height: '56px',
-    background: '#29B3BC',
+    background: '#A7DBD6',
     top: '0',
     position: 'relative',
   },
@@ -55,11 +55,11 @@ export const statsStyling = {
 export const globalStatsData = [
   // A maximum of 6 stats are allowed
   {
-    statTitle: 'Studies',
+    statTitle: 'Diagnoses',
     type: 'field',
-    statAPI: 'numberOfStudies',
-    statIconSrc: studiesLogo,
-    statIconAlt: 'An open notebook representing studies',
+    statAPI: 'numberOfParticipants',
+    statIconSrc: diagnosisLogo,
+    statIconAlt: 'A teddy bear representing Diagnosis'
   },
   {
     statTitle: 'Participants',
@@ -69,26 +69,195 @@ export const globalStatsData = [
     statIconAlt: 'A teddy bear representing participants'
   },
   {
-    statTitle: 'Samples',
+    statTitle: 'Studies',
     type: 'field',
-    statAPI: 'numberOfSamples',
-    statIconSrc: samplesLogo,
-    statIconAlt: 'A test tube holder with three test tubes in the holder representing samples'
-  },
-  {
-    statTitle: 'Files',
-    type: 'field',
-    statAPI: 'numberOfFiles',
-    statIconSrc: fileLogo,
-    statIconAlt: 'An open file drawer representing files'
+    statAPI: 'numberOfParticipants',
+    statIconSrc: studiesLogo,
+    statIconAlt: 'A teddy bear representing Studies'
   },
 ];
 
 // --------------- GraphQL query - Retrieve stats details --------------
-export const GET_GLOBAL_STATS_DATA_QUERY = gql`{
-  numberOfFiles
+export const GET_GLOBAL_STATS_DATA_QUERY = gql`
+query search(
+  # Demographics
+  $participant_ids: [String],
+  $ethnicity: [String],
+  $race: [String],
+  $sex_at_birth: [String],
+
+  # Diagnoses
+  $age_at_diagnosis: [Int],
+  $anatomic_site: [String],
+  $diagnosis_classification: [String],
+  $diagnosis_classification_system: [String],
+  $diagnosis_verification_status: [String],
+  $diagnosis_basis: [String],
+  $disease_phase: [String],
+
+  # Studies
+  $phs_accession: [String],
+  $study_acronym: [String],
+  $study_short_title: [String],
+
+  # Survivals
+  $age_at_last_known_survival_status: [Int],
+  $first_event: [String],
+  $last_known_survival_status: [String]
+) {
+getParticipants(
+  # Demographics
+  participant_ids: $participant_ids,
+  ethnicity: $ethnicity,
+  race: $race,
+  sex_at_birth: $sex_at_birth,
+
+  # Diagnoses
+  age_at_diagnosis: $age_at_diagnosis,
+  anatomic_site: $anatomic_site,
+  diagnosis_classification: $diagnosis_classification,
+  diagnosis_classification_system: $diagnosis_classification_system,
+  diagnosis_verification_status: $diagnosis_verification_status,
+  diagnosis_basis: $diagnosis_basis,
+  disease_phase: $disease_phase,
+  
+  # Studies
+  phs_accession: $phs_accession,
+  study_acronym: $study_acronym,
+  study_short_title: $study_short_title,
+
+  # Survivals
+  age_at_last_known_survival_status: $age_at_last_known_survival_status,
+  first_event: $first_event,
+  last_known_survival_status: $last_known_survival_status
+) {
   numberOfParticipants
-  numberOfSamples
+  numberOfDiagnoses
   numberOfStudies
+
+  # Widget counts
+  participantCountByEthnicity {
+    group
+    subjects
+    __typename
   }
+  participantCountByRace {
+    group
+    subjects
+    __typename
+  }
+  participantCountBySexAtBirth {
+    group
+    subjects
+    __typename
+  }
+  participantCountByAgeAtLastKnownSurvivalStatus {
+    group
+    subjects
+    __typename
+  }
+  participantCountByFirstEvent {
+    group
+    subjects
+    __typename
+  }
+  participantCountByLastKnownSurvivalStatus {
+    group
+    subjects
+    __typename
+  }
+
+  # Demographic filter counts
+  filterParticipantCountByEthnicity {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByRace {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountBySexAtBirth {
+    group
+    subjects
+    __typename
+  }
+
+  # Diagnosis filter counts
+  filterParticipantCountByAgeAtDiagnosis {
+    lowerBound
+    upperBound
+    subjects
+    __typename
+  }
+  filterParticipantCountByAnatomicSite {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByDiagnosisClassification {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByDiagnosisClassificationSystem {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByDiagnosisVerificationStatus {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByDiagnosisBasis {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByDiseasePhase {
+    group
+    subjects
+    __typename
+  }
+
+  # Study filter counts
+  filterParticipantCountByPhsAccession {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByStudyAcronym {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByStudyShortTitle {
+    group
+    subjects
+    __typename
+  }
+
+  # Survival filter counts
+  filterParticipantCountByAgeAtLastKnownSurvivalStatus {
+    lowerBound
+    upperBound
+    subjects
+    __typename
+  }
+  filterParticipantCountByFirstEvent {
+    group
+    subjects
+    __typename
+  }
+  filterParticipantCountByLastKnownSurvivalStatus {
+    group
+    subjects
+    __typename
+  }
+  __typename
+}
+}
+
   `;
