8e01d68ef0c0ffeca020c12ba8ee647f4c1ca6d6
Senait7-ux <119904111+Senait7-ux@users.noreply.github.com>
2024-02-27 11:42:04 -0500

    Data Model page Implementation

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -11,6 +11,7 @@ import Search from '../../pages/search/searchView';
 import Inventory from '../../pages/inventory/inventoryController';
 import Cart from '../../pages/cart/cartController';
 import ScrollButton from '../ScrollButton/ScrollButtonView';
+import DataModel from "../../pages/DataModel/dataModel";
 // import NewsDetail from '../../pages/news/newsDetailView';
 
 const Layout = () => {
@@ -26,6 +27,7 @@ const Layout = () => {
           <Route path="/sitesearch" element={<Search />} />
           <Route path="/explore" element={<Inventory />} />
           <Route path="/fileCentricCart" element={<Cart />} />
+          <Route path="/data_model" element={<DataModel />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
