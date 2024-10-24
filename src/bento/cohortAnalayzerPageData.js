5614bfac6c2300f0f024278c2f6da352b3f2b2a5
Senait7-ux <119904111+Senait7-ux@users.noreply.github.com>
2024-10-24 13:44:25 -0400

    * C3DC-1124 implementation

---
+++ src/bento/cohortAnalayzerPageData.js
@@ -0,0 +1,79 @@
+import { cellTypes } from "@bento-core/table";
+import { GET_COHORT_MANIFEST_QUERY } from "./dashboardTabData";
+
+export const tableConfig =  {
+    name: 'Participants',
+    dataField: 'dataParticipant',
+    api: GET_COHORT_MANIFEST_QUERY,
+    paginationAPIField: 'diagnosisOverview',
+    count: 'numberOfParticipants',
+    fileCount: 'participantsFileCount',
+    dataKey: 'participant_id',
+    hiddenDataKeys: ['participant_id', 'participant_pk', 'dbgap_accession'],
+    defaultSortField: 'participant_id',
+    defaultSortDirection: 'asc',
+    toolTipText: 'Count of Participant Record',
+    buttonText: 'Add Selected Files',
+    tableID: 'participant_tab_table',
+    hasToolTip: true,
+    extendedViewConfig: {
+      pagination: true,
+      manageViewColumns: false,
+      download: true,
+      downloadButtonConfig: {
+        title: 'DOWNLOAD DATA',
+        cloudIcon: true,
+      },
+    },
+    columns: [
+      {
+        dataField: 'participant_id',
+        header: 'Participant Id',
+        display: true,
+        tooltipText: 'sort',
+        role: cellTypes.DISPLAY,
+      },
+      {
+        dataField: 'race',
+        header: 'Race',
+        display: true,
+        tooltipText: 'sort',
+        role: cellTypes.DISPLAY,
+      },
+      {
+        dataField: 'sex_at_birth',
+        header: 'Sex at Birth',
+        display: true,
+        tooltipText: 'sort',
+        role: cellTypes.DISPLAY,
+      },
+      {
+        dataField: 'dbgap_accession',
+        header: 'dbGaP Accession',
+        display: true,
+        tooltipText: 'sort',
+        role: cellTypes.DISPLAY,
+        linkAttr: {
+          linkAttr: {
+            rootPath: 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=',
+          },
+          cellType: cellTypes.CUSTOM_ELEM,
+        },
+        doNotDownload: true,
+      },
+      {
+        dataField: "study_id",
+        header: "Study Id",
+        display: false,
+        tooltipText: "sort",
+        role: cellTypes.DISPLAY
+      }
+    ],
+    id: 'participant_tab',
+    tableDownloadCSV: {},
+    tabIndex: '0',
+    downloadFileName: 'C3DC Participants Download',
+    tableMsg: {
+      noMatch: 'To proceed, please select a cohort from the Cohort List (Left Panel).',
+    },
+  };
---
+++ src/pages/CohortAnalyzer/downloadCohort/DownloadSelectedCohorts.js
@@ -0,0 +1,162 @@
+import { Button, makeStyles } from '@material-ui/core';
+import React, { useRef, useState } from 'react';
+import ExpandMoreIcon from '../../../assets/icons/Expand_More_Icon.svg'
+import { arrayToCSVDownload, objectToJsonDownload } from '../../inventory/cohortModal/utils';
+import client from '../../../utils/graphqlClient';
+import { GET_COHORT_MANIFEST_QUERY, GET_COHORT_METADATA_QUERY } from '../../../bento/dashboardTabData';
+
+export default function DownloadSelectedCohort({ queryVariable, isSelected }) {
+
+    const [showDownloadDropdown, setShowDownloadDropdown] = useState(false);
+   
+    const handleDownloadFile = (download) => {
+        download();
+        setShowDownloadDropdown(false);
+    };
+
+    const handleDownloadDropdown = () => {
+        if (isSelected) {
+            setShowDownloadDropdown(!showDownloadDropdown);
+        }
+    }
+    
+    const downloadCohortManifest = async () => {
+        const { data } = await client.query({
+            query: GET_COHORT_MANIFEST_QUERY,
+            variables: queryVariable,
+        });
+        arrayToCSVDownload(data['diagnosisOverview'], "analyzed");
+    };
+
+    const downloadCohortMetadata = async () => {
+        const { data } = await client.query({
+            query: GET_COHORT_METADATA_QUERY,
+            variables: queryVariable,
+        });
+        objectToJsonDownload(data['cohortMetadata'], "Analyzed");
+    };
+
+    const classes = useStyles({ isSelected });
+    const dropdownRef = useRef(null);
+
+    return (
+        <div className={classes.dropdownSection} ref={dropdownRef}>
+            <Button
+                variant="contained"
+                className={showDownloadDropdown ? classes.downloadButtonOpened : classes.downloadButton}
+                onClick={handleDownloadDropdown}
+            >
+                <div className={classes.downloadButtonText}>
+                    <span>Download</span>
+                    <span>Selected Cohorts</span>
+                </div>
+                <img
+                    src={ExpandMoreIcon}
+                    alt="expand download icon"
+                    className={`${showDownloadDropdown ? classes.rotatedIcon : ''}`}
+                />
+            </Button>
+            {showDownloadDropdown && (
+                <div className={classes.dropdownMenu}>
+                    <div
+                        className={classes.dropdownItem + ' ' + classes.firstDropdownItem}
+                        onClick={() => { handleDownloadFile(downloadCohortManifest) }}
+                    >
+                        Manifest CSV
+                    </div>
+                    <div
+                        className={classes.dropdownItem}
+                        onClick={() => { handleDownloadFile(downloadCohortMetadata) }}
+                    >
+                        Metadata JSON
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+const useStyles = makeStyles((theme) => ({
+    downloadButton: (props) => ({
+        backgroundColor: props.isSelected ? '#0C534C' : '#556469',
+        border: '1.25px solid #73A9C7',
+        width: '189px',
+        color: 'white',
+        borderTopLeftRadius: '5px',
+        borderTopRightRadius: '5px',
+        borderBottomLeftRadius: '5px',
+        borderBottomRightRadius: '5px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        lineHeight: '13px !important',
+        zIndex: '1',
+        boxShadow: 'none',
+        '&:hover': {
+            backgroundColor: props.isSelected ? '#003B35' : '#556469',
+            boxShadow: 'none',
+        },
+    }),
+    rotatedIcon: {
+        transform: 'rotate(180deg)'
+    },
+    downloadButtonOpened: {
+        backgroundColor: '#0C534C',
+        border: '1.25px solid #73A9C7',
+        width: '189px',
+        color: 'white',
+        borderTopLeftRadius: '5px',
+        borderTopRightRadius: '5px',
+        borderBottomLeftRadius: '0px',
+        borderBottomRightRadius: '0px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        lineHeight: '13px !important',
+        zIndex: '1',
+        boxShadow: 'none',
+        '&:hover': {
+            backgroundColor: '#003B35',
+            boxShadow: 'none',
+        },
+    },
+    downloadButtonText: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'start !important',
+        fontFamily: 'Poppins',
+        fontSize: '12px',
+        fontWeight: 600,
+
+
+    },
+    dropdownSection: {
+        position: 'relative',
+    },
+    dropdownMenu: {
+        position: 'absolute',
+        top: '39.5px',
+        width: '189px',
+        backgroundColor: '#EFF2F6',
+        border: '1px solid #0C534C',
+        borderBottomLeftRadius: '5px',
+        borderBottomRightRadius: '5px',
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'hidden',
+        fontFamily: 'Poppins',
+        fontSize: '12px',
+        lineHeight: '16px',
+        fontWeight: '500',
+        zIndex: 1,
+        '& div': {
+            padding: '13.2px 21px',
+            '&:hover': {
+                backgroundColor: '#CCD5E1',
+                cursor: 'pointer',
+            },
+        },
+    },
+    firstDropdownItem: {
+        borderBottom: '1px solid #0C534C',
+    }
+}));
