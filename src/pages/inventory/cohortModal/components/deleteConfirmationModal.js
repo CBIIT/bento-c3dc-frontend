0b835312db06f07138306be8f0aaa8330f567762
huuaho <32457341+huuaho@users.noreply.github.com>
2024-12-05 01:54:56 -0500

    updated verbiage for unsaved changes.

--- src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
+++ src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Modal, withStyles } from '@material-ui/core';
 
 export const deletionTypes = {
-    DELETE_ALL_COHORTS: 'ALL cohorts?',
-    DELETE_SINGLE_COHORT: 'this cohort?',
-    DELETE_ALL_PARTICIPANTS: 'ALL participants?',
+    DELETE_ALL_COHORTS: 'delete ALL cohorts?',
+    DELETE_SINGLE_COHORT: 'delete this cohort?',
+    DELETE_ALL_PARTICIPANTS: 'delete ALL participants?',
     CLEAR_UNSAVED_CHANGES: 'leave? You will lose all unsaved changes.'
 };
 
@@ -29,7 +29,7 @@ const DeleteConfirmationModal = (props) => {
                             <span>{message}</span>
                             :
                             <>
-                                <span> Are you sure you want to delete {deletionType} </span>
+                                <span> Are you sure you want to {deletionType} </span>
                                 {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>This action cannot be undone. Press Confirm or Cancel. </span>}
                             </>
                         }
