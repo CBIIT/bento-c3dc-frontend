32ab729677df225f7758b1a879b433d5e8d3f5ab
huuaho <32457341+huuaho@users.noreply.github.com>
2024-12-05 11:54:50 -0500

    updated text for delete confirmation modal to be 3 lines always

--- src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
+++ src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Modal, withStyles } from '@material-ui/core';
 
 export const deletionTypes = {
-    DELETE_ALL_COHORTS: 'delete ALL cohorts?',
+    DELETE_ALL_COHORTS: 'delete all cohorts?',
     DELETE_SINGLE_COHORT: 'delete this cohort?',
-    DELETE_ALL_PARTICIPANTS: 'delete ALL participants?',
-    CLEAR_UNSAVED_CHANGES: 'leave? You will lose all unsaved changes.'
+    DELETE_ALL_PARTICIPANTS: 'delete all participants?',
+    CLEAR_UNSAVED_CHANGES: 'leave?'
 };
 
 const DeleteConfirmationModal = (props) => {
@@ -30,7 +30,8 @@ const DeleteConfirmationModal = (props) => {
                             :
                             <>
                                 <span> Are you sure you want to {deletionType} </span>
-                                {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>This action cannot be undone. Press Confirm or Cancel. </span>}
+                                {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>{deletionType === deletionTypes.CLEAR_UNSAVED_CHANGES && 'You will lose all unsaved changes. ' }This action cannot be undone.</span>}
+                                {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>Press Confirm or Cancel. </span>}
                             </>
                         }
 
