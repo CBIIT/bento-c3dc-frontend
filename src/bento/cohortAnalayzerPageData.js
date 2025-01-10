b217b9e791dd1824faf8c411f5377a3626ccfd0f
Senait7-ux <119904111+Senait7-ux@users.noreply.github.com>
2025-01-10 12:50:38 -0500

    * radio buttons for table added

--- src/bento/cohortAnalayzerPageData.js
+++ src/bento/cohortAnalayzerPageData.js
@@ -1,5 +1,6 @@
 import { cellTypes } from "@bento-core/table";
 import { GET_COHORT_MANIFEST_QUERY } from "./dashboardTabData";
+import gql from "graphql-tag";
 
 export const tableConfig = {
   name: 'Participants',
@@ -85,3 +86,237 @@ export const tableConfig = {
     noMatch: 'To proceed, please select a cohort from the Cohort List (Left Panel).',
   },
 };
+
+export const diagnosesTableConfig= 
+{
+  name: 'Participants',
+  dataField: 'dataParticipant',
+  api: GET_COHORT_MANIFEST_QUERY,
+  paginationAPIField: 'diagnosisOverview',
+  count: 'numberOfParticipants',
+  fileCount: 'participantsFileCount',
+  dataKey: 'participant_id',
+  hiddenDataKeys: ['participant_id', 'participant_pk', 'dbgap_accession'],
+  defaultSortField: 'participant_id',
+  defaultSortDirection: 'asc',
+  toolTipText: 'Count of Participant Record',
+  buttonText: 'Add Selected Files',
+  tableID: 'participant_tab_table',
+  hasToolTip: true,
+  extendedViewConfig: {
+    pagination: true,
+    manageViewColumns: false,
+    download: true,
+    downloadButtonConfig: {
+      title: 'DOWNLOAD DATA',
+      cloudIcon: true,
+    },
+  },
+  columns: [
+    {
+      dataField: 'participant_id',
+      header: 'Participant Id',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: 'age_at_diagnosis',
+      header: 'Age At Diagnosis',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: 'anatomic_site',
+      header: 'Anatomic Sites',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: "cohort",
+      header: "Cohorts",
+      display: true,
+      tooltipText: "This entry is found in the following cohorts",
+      role: cellTypes.DISPLAY,
+      cellType: cellTypes.CUSTOM_ELEM
+    }
+  ],
+  id: 'participant_tab',
+  tableDownloadCSV: {},
+  tabIndex: '0',
+  downloadFileName: 'C3DC Participants Download',
+  tableMsg: {
+    noMatch: 'To proceed, please select a cohort from the Cohort List (Left Panel).',
+  },
+}
+export const treatmentTableConfig = {
+  name: 'Participants',
+  dataField: 'dataParticipant',
+  api: GET_COHORT_MANIFEST_QUERY,
+  paginationAPIField: 'diagnosisOverview',
+  count: 'numberOfParticipants',
+  fileCount: 'participantsFileCount',
+  dataKey: 'participant_id',
+  hiddenDataKeys: ['participant_id', 'participant_pk', 'dbgap_accession'],
+  defaultSortField: 'participant_id',
+  defaultSortDirection: 'asc',
+  toolTipText: 'Count of Participant Record',
+  buttonText: 'Add Selected Files',
+  tableID: 'participant_tab_table',
+  hasToolTip: true,
+  extendedViewConfig: {
+    pagination: true,
+    manageViewColumns: false,
+    download: true,
+    downloadButtonConfig: {
+      title: 'DOWNLOAD DATA',
+      cloudIcon: true,
+    },
+  },
+  columns: [
+    {
+      dataField: 'participant_id',
+      header: 'Participant Id',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: 'treatment_type',
+      header: 'Treatment Type',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: 'treatment_agent',
+      header: 'Treatment Agent',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },{
+      dataField: 'treatment_pk',
+      header: 'Treatment Id',
+      display: true,
+      tooltipText: 'sort',
+      role: cellTypes.DISPLAY,
+    },
+    {
+      dataField: "cohort",
+      header: "Cohorts",
+      display: true,
+      tooltipText: "This entry is found in the following cohorts",
+      role: cellTypes.DISPLAY,
+      cellType: cellTypes.CUSTOM_ELEM
+    }
+  ],
+  id: 'participant_tab',
+  tableDownloadCSV: {},
+  tabIndex: '0',
+  downloadFileName: 'C3DC Participants Download',
+  tableMsg: {
+    noMatch: 'To proceed, please select a cohort from the Cohort List (Left Panel).',
+  },
+};
+
+
+const participant_query = gql`query participantOverview(
+    # Demographics
+    $participant_pks: [String],
+
+    # Table config
+    $first: Int,
+    $offset: Int,
+    $order_by: String,
+    $sort_direction: String
+) {
+participantOverview(
+    # Demographics
+    participant_pks: $participant_pks,
+
+    # Table config
+    first: $first,
+    offset: $offset,
+    order_by: $order_by,
+    sort_direction: $sort_direction
+) { 
+    # Participants
+    participant_pk
+    participant_id
+    race
+    sex_at_birth
+
+    # Studies
+    dbgap_accession
+
+    __typename
+}}`;
+const diagnosis_query = gql`query diagnosisOverview(
+    # Demographics
+    $participant_pks: [String]
+
+    # Table config
+    $first: Int,
+    $offset: Int,
+    $order_by: String,
+    $sort_direction: String
+) {
+diagnosisOverview(
+    # Demographics
+    participant_pks: $participant_pks,
+
+    # Table config
+    first: $first,
+    offset: $offset,
+    order_by: $order_by,
+    sort_direction: $sort_direction
+) {
+    # Demographics
+    participant_id
+participant_pk
+    # Diagnosis
+    diagnosis_pk
+    age_at_diagnosis
+    anatomic_site
+    diagnosis
+
+    __typename
+}}`;
+const treatment_query = gql`query treatmentOverview(
+    # Demographics
+    $participant_pks: [String],
+
+    # Table config
+    $first: Int,
+    $offset: Int,
+    $order_by: String,
+    $sort_direction: String
+) {
+treatmentOverview(
+    # Demographics
+    participant_pks: $participant_pks,
+
+    # Table config
+    first: $first,
+    offset: $offset,
+    order_by: $order_by,
+    sort_direction: $sort_direction
+) {
+    # Participant
+    participant_id
+   participant_pk 
+    # Treatment
+    treatment_pk
+    treatment_type
+    treatment_agent
+    treatment_agent_str
+
+    __typename
+}}
+`;
+
+export const analyzer_query = [participant_query,diagnosis_query,treatment_query];
+export const analyzer_tables = [tableConfig,diagnosesTableConfig,treatmentTableConfig];
+export const responseKeys = ["participantOverview","diagnosisOverview","treatmentOverview"];
