d24eea7a0a2697c93236aa4bdb3c7679ad093515
Ankerso <ankersosed@nci-02346685-ml.nih.gov>
2025-04-16 17:44:12 -0400

    Adjusted font weight

--- src/pages/inventory/cohortModal/components/cohortDetails.js
+++ src/pages/inventory/cohortModal/components/cohortDetails.js
@@ -242,21 +242,21 @@ setTooltipOpen(false)
         ? config.cohortCountsLabel
         : DEFAULT_CONFIG.config.cohortDetails.cohortCountsLabel;
 
-    const exploreCCDIHubTooltip = <p style={{fontFamily:"Poppins", zIndex: 10000}}>
-         Clicking this button will create a url and open a new tab showing the  CCDI Hub  Explore page with filtered facets based on the user&apos;s selected  cohort.
-         <br/><br/>
-         <b>If cohort size &le; 600:</b><br/> 
-Proceed with direct export within C3DC.
-<br/><br/>
-            <b>If cohort size &gt; 600:</b><br/> 
-            Download the manifest and upload it manually to the <a style={{zIndex: 10000}} target='_blank' href="https://ccdi.cancer.gov/explore"> CCDI Hub 
-            <img src={LinkoutBlue} width={14} height={14} style={{padding: "4px 0px 0px 2px", bottom: 0, position: 'relative'}} alt="Linkout Icon" /> 
-            </a> by following these steps:
-<ol style={{ paddingLeft: "1rem"}}>
-<li> Choose the Explore page from the menu.</li>
-<li> In the Facets side panel, open the Demographic facet.</li>
-<li> Click on “Upload Participants Set.”</li>
-</ol>
+    const exploreCCDIHubTooltip = <p style={{ fontFamily: "Poppins", zIndex: 10000, fontWeight:400 }}>
+        Clicking this button will create a url and open a new tab showing the  CCDI Hub  Explore page with filtered facets based on the user&apos;s selected  cohort.
+        <br /><br />
+        <b>If cohort size &le; 600:</b><br />
+        Proceed with direct export within C3DC.
+        <br /><br />
+        <b>If cohort size &gt; 600:</b><br />
+        Download the manifest and upload it manually to the <a style={{ zIndex: 10000 }} target='_blank' href="https://ccdi.cancer.gov/explore"> CCDI Hub
+            <img src={LinkoutBlue} width={14} height={14} style={{ padding: "4px 0px 0px 2px", bottom: 0, position: 'relative' }} alt="Linkout Icon" />
+        </a> by following these steps:
+        <ol style={{ paddingLeft: "1rem" }}>
+            <li> Choose the Explore page from the menu.</li>
+            <li> In the Facets side panel, open the Demographic facet.</li>
+            <li> Click on “Upload Participants Set.”</li>
+        </ol>
     </p>;
 
     return (
