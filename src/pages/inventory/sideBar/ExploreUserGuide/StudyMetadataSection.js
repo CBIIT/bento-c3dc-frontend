b198c97e7789c19ca310686907cb4cca592c1beb
Karan Sheth <karan.sheth60@gmail.com>
2025-07-08 16:56:40 -0400

    Compeleted Section 4: Downloading Participant Metadata and Harmonized Clinical Data.

--- src/pages/inventory/sideBar/ExploreUserGuide/StudyMetadataSection.js
+++ src/pages/inventory/sideBar/ExploreUserGuide/StudyMetadataSection.js
@@ -1,33 +1,22 @@
-// components/ExploreUserGuide/StudyMetadataSection.js
 import React from 'react';
-import figure10 from '../../../../assets/explore/C3DCFacetLocalFindSearchIcon.svg';
-import figure11 from '../../../../assets/explore/C3DCFacetLocalFindSearchIcon.svg';
+import figure7 from "./images/figure7.png";;
 
 const StudyMetadataSection = ({ classes }) => (
   <div>
     <div id='Downloading Metadata from the Studies tab' className={classes.sectionTitle}>
-      <p>Downloading Metadata from the Studies tab</p>
+      <p>Downloading Participant Metadata and Harmonized Clinical Data</p>
     </div>
     <div className={classes.contentContainer}>
       <p>
-        From the CCDI Hub Explore Dashboard, you can download all open metadata for each study from the “Studies” tab to further filter data and build cohorts. For example, you can filter by diagnosis to generate a set of participants and upload the manifest into the CGC.
+        Users can download the table contents of the <strong>Studies, Participants, Diagnosis, Treatment, Treatment Response</strong>, and <strong>Survival</strong> tabs by selecting the <strong>"Download Data"</strong> button under the table tab headers (Figure 5).
+        Filtered data can be downloaded in either <code>CSV</code> or <code>JSON</code> format.
       </p>
-      <ol>
-        <li>Open the “STUDY” filters from the left menu, expand “STUDY NAME,” and select “Molecular Characterization Initiative.”</li>
-        <li>The dashboard will reload, filtered for this study.</li>
-        <li>Navigate to the “Studies” tab and locate the “Manifest” column.</li>
-        <li>
-          Click the “Download study manifest” icon (Figure 10).
-          <div className={classes.figureContainer}><img src={figure10} style={{ width: '90%' }} alt='Figure10' /></div>
-          <div className={classes.figureText}>Figure 10: Download metadata manifest for a given study</div>
-        </li>
-        <li>
-          Open the downloaded file on your computer to view metadata (Figure 11).
-          <div className={classes.figureContainer}><img src={figure11} style={{ width: '80%' }} alt='Figure11' /></div>
-          <div className={classes.figureText}>Figure 11: Study metadata export file browsable on local machine</div>
-        </li>
-      </ol>
-      <p>Appendix A details how to generate a DRS manifest from this metadata to use in CGC.</p>
+      <div className={classes.figureContainer}>
+        <img src={figure7} style={{ width: '80%' }} alt='Figure 5' />
+      </div>
+      <div className={classes.figureText}>
+        Figure 7: Download Data buttons feature two different download formats, CSV or JSON
+      </div>
     </div>
   </div>
 );
