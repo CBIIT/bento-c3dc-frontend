56a693c31bc0cd792cd5d6d4b6e6d4f59bedd759
huuaho <32457341+huuaho@users.noreply.github.com>
2025-07-23 20:27:33 -0400

    export complex modal component logic into hooks

--- src/pages/inventory/cohortModal/CohortModal.js
+++ src/pages/inventory/cohortModal/CohortModal.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-import { CohortStateContext } from '../../../components/CohortSelectorState/CohortStateContext.js';
 import {
     Modal, withStyles,
 } from '@material-ui/core';
@@ -8,10 +7,10 @@ import DEFAULT_CONFIG from './config';
 import CohortList from './components/cohortList';
 import CohortDetails from './components/cohortDetails';
 import DeleteConfirmationModal from './components/deleteConfirmationModal';
-import { deletionTypes } from './components/deleteConfirmationModal';
 import AlertManager from './components/AlertManager';
-import { hasUnsavedChanges } from './utils.js';
-import { CohortModalContext } from './CohortModalContext.js'
+import { CohortModalContext } from './CohortModalContext.js';
+import { useModalState } from './hooks/useModalState';
+import { useUnsavedChanges } from './hooks/useUnsavedChanges';
 
 /**
  * CohortModal component for managing cohorts with custom configuration support.
@@ -29,26 +28,20 @@ const CohortModal = (props) => {
     } = props;
 
     const { 
-        currentCohortChanges, 
-        clearCurrentCohortChanges, 
-        selectedCohort, 
-        setSelectedCohort,
+        clearCurrentCohortChanges,
         showDeleteConfirmation,
         setShowDeleteConfirmation,
-        deleteModalProps,
-        setDeleteModalProps
+        deleteModalProps
     } = useContext(CohortModalContext) || {};
-    const { state } = useContext(CohortStateContext) || {};
-    const ignoredFields = ["cohortId"]
-    const unSavedChanges = currentCohortChanges ? hasUnsavedChanges(currentCohortChanges, state[selectedCohort], ignoredFields) : false;
 
-    const modalClosed = functions && typeof functions.modalClosed === 'function'
-        ? functions.modalClosed
-        : DEFAULT_CONFIG.functions.modalClosed;
+    // Use custom hooks for complex logic
+    const { unSavedChanges } = useUnsavedChanges();
+    const { unSavedChangesCheck } = useModalState(
+        modalProps.onCloseModal,
+        functions.modalClosed || DEFAULT_CONFIG.functions.modalClosed
+    );
 
-    const modalTitle = config && config.title && typeof config.title === 'string'
-        ? config.title
-        : DEFAULT_CONFIG.config.title;
+    const modalTitle = config.title || DEFAULT_CONFIG.config.title;
     const { open } = modalProps;
 
     const {
@@ -57,26 +50,7 @@ const CohortModal = (props) => {
         DeleteConfirmation: deleteConfirmationClasses,
     } = classes;
 
-    const closeModalWrapper = () => {
-        modalClosed();
-        if (modalProps.onCloseModal) {
-            modalProps.onCloseModal();
-        }
-        setSelectedCohort(null);
-    };
-
-    const unSavedChangesCheck = () => {
-        if (unSavedChanges) {
-            setDeleteModalProps({
-                handleDelete: () => closeModalWrapper(),
-                deletionType: deletionTypes.CLEAR_UNSAVED_CHANGES,
-            });
-            setShowDeleteConfirmation(true)
-        }
-        else {
-            closeModalWrapper()
-        }
-    }
+    const handleModalClose = () => unSavedChangesCheck(unSavedChanges);
 
     useEffect(() => {
         if (!open) {
@@ -90,12 +64,12 @@ const CohortModal = (props) => {
                 {...modalProps}
                 open={open}
                 className={classes.modal}
-                onClose={unSavedChangesCheck}
+                onClose={handleModalClose}
             >
                         <div className={classes.paper}>
                             <h1 className={classes.modalTitle}>
                                 <span>{modalTitle}</span>
-                                <span className={classes.closeIcon} onClick={unSavedChangesCheck}>
+                                <span className={classes.closeIcon} onClick={handleModalClose}>
                                     <img
                                         src="https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/svgs/LocalFindCaseDeleteIcon.svg"
                                         alt="close icon"
@@ -109,12 +83,12 @@ const CohortModal = (props) => {
                                     classes={cohortListClasses}
                                     config={config.cohortList}
                                     unSavedChanges={unSavedChanges}
-                                    closeModal={unSavedChangesCheck}
+                                    closeModal={handleModalClose}
                                 />
                                 <CohortDetails
                                     classes={cohortDetailsClasses}
                                     config={config.cohortDetails}
-                                    closeModal={unSavedChangesCheck}
+                                    closeModal={handleModalClose}
                                 />
                             </div>
                         </div>
