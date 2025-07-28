a682d29c1e6111470d5f23d190d64814e1e7be20
Ankerso <ankersosed@nci-02346685-ml.nih.gov>
2025-07-28 15:51:23 -0400

    * Comments addressed

--- src/pages/CohortAnalyzer/vennDiagram/ChartVennConfig.js
+++ src/pages/CohortAnalyzer/vennDiagram/ChartVennConfig.js
@@ -1,6 +1,6 @@
 // Default threshold for font size adjustment in the chart. 
 // The value 999 was chosen as a high threshold to ensure font size adjustments are only applied in extreme cases.
-// This threshold was determined based on observed data visualization needs. such as overlapping numbers when numbers are above 999
+// This threshold was determined based on observed data visualization needs, such as overlapping numbers when numbers are above 999.
 export const DEFAULT_FONT_SIZE_THRESHOLD = 999;
 export const nodes = ["participant_pk","diagnosis","treatment_type"];
 
@@ -11,4 +11,9 @@ export const hexToRgba = (hex, alpha = 1) => {
   return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${alpha})`;
 };
 
-export const baseColorArray = ["#F9E28B", "#86E2B9", "#5198C8"].map((color,index) => hexToRgba(color,index === 2 ? 0.85 : 1));
+// Constants for color opacity adjustments
+const REDUCED_OPACITY = 0.85; // Opacity for the third color to make it visually distinct
+const THIRD_COLOR_INDEX = 2; // Index of the third color in the array
+export const baseColorArray = ["#F9E28B", "#86E2B9", "#5198C8"].map((color, index) => 
+  hexToRgba(color, index === THIRD_COLOR_INDEX ? REDUCED_OPACITY : 1)
+);
