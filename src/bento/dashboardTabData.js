ed300c36b779141cb337c05260e85d36ab32408c
huuaho <32457341+huuaho@users.noreply.github.com>
2025-09-10 11:35:05 -0400

    updated metadata query

--- src/bento/dashboardTabData.js
+++ src/bento/dashboardTabData.js
@@ -321,86 +321,93 @@ getParticipants(
 
 export const GET_COHORT_METADATA_QUERY = gql`
 query cohortMetadata(
-    # Demographics
-    $participant_pk: [String],
+  # Demographics
+  $participant_pk: [String],
 
-    # Table config
-    $first: Int,
-    $offset: Int,
-    $order_by: String,
-    $sort_direction: String
+  # Table config
+  $first: Int,
+  $offset: Int,
+  $order_by: String,
+  $sort_direction: String
 ) {
 cohortMetadata(
-    # Demographics
-    participant_pk: $participant_pk,
+  # Demographics
+  participant_pk: $participant_pk,
 
-    # Table config
-    first: $first,
-    offset: $offset,
-    order_by: $order_by,
-    sort_direction: $sort_direction
+  # Table config
+  first: $first,
+  offset: $offset,
+  order_by: $order_by,
+  sort_direction: $sort_direction
 ) {
-    dbgap_accession
+  dbgap_accession
+
+  consent_groups {
+    consent_group_name
+    consent_group_number
 
     participants {
-        id 
-        participant_id
-        race
-        sex_at_birth
-      
-        diagnoses {
-            id 
-            diagnosis_id
-            age_at_diagnosis
-            anatomic_site
-            diagnosis
-            diagnosis_basis
-            diagnosis_classification_system
-            diagnosis_comment
-            disease_phase
-            toronto_childhood_cancer_staging
-            tumor_classification
-            tumor_grade
-            tumor_stage_clinical_m
-            tumor_stage_clinical_n
-            tumor_stage_clinical_t
-        }
-        survivals {
-            id 
-            survival_id
-            age_at_event_free_survival_status
-            age_at_last_known_survival_status
-            cause_of_death
-            event_free_survival_status
-            first_event
-            last_known_survival_status
-        }
-        synonyms {
-          id
-          associated_id
-          data_location
-          domain_category
-          domain_description
-          repository_of_synonym_id
-        }
-        treatments {
-            id 
-            treatment_id
-            age_at_treatment_end
-            age_at_treatment_start
-            treatment_agent
-            treatment_type
-        }
-        treatment_responses {    id 
-            treatment_response_id
-            age_at_response
-            response
-            response_category
-            response_system
-        }
+      id
+      participant_id
+      race
+      sex_at_birth
+
+      diagnoses {
+        id
+        diagnosis_id
+        age_at_diagnosis
+        anatomic_site
+        diagnosis
+        diagnosis_basis
+        diagnosis_classification_system
+        diagnosis_comment
+        disease_phase
+        toronto_childhood_cancer_staging
+        tumor_classification
+        tumor_grade
+        tumor_stage_clinical_m
+        tumor_stage_clinical_n
+        tumor_stage_clinical_t
+      }
+      survivals {
+        id
+        survival_id
+        age_at_event_free_survival_status
+        age_at_last_known_survival_status
+        cause_of_death
+        event_free_survival_status
+        first_event
+        last_known_survival_status
+      }
+      synonyms {
+        id
+        associated_id
+        data_location
+        domain_category
+        domain_description
+        repository_of_synonym_id
+      }
+      treatments {
+        id
+        treatment_id
+        age_at_treatment_end
+        age_at_treatment_start
+        treatment_agent
+        treatment_type
+      }
+      treatment_responses {
+        id
+        treatment_response_id
+        age_at_response
+        response
+        response_category
+        response_system
+      }
     }
+  }
 }}
 `;
+
 export const DISPLAY_COHORT_QUERY = gql`
 query participantOverview(
     # Demographics
