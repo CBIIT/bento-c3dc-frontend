1dcdd33bf33f9fd39ff460bfbe911dfa1fc28d58
Ankerso <ankersosed@nci-02346685-ml.nih.gov>
2025-10-07 15:14:34 -0400

    Release Notes PDF for Release 7

--- src/bento/aboutPageData.js
+++ src/bento/aboutPageData.js
@@ -1,5 +1,5 @@
 import userGuide from '../assets/releaseNotes/C3DC User Guide-R6-V1.pdf';
-import latestRelease from'../assets/releaseNotes/C3DC_Release_Notes_R6.pdf';
+import latestRelease from'../assets/releaseNotes/C3DC_Release_Notes_R7.pdf';
 
 export const aboutData = {
   aboutHeaderText:
