b6622f5523215ce675474481b2cb6769099c4d31
Senait7-ux <senaitankerso@gmail.com>
2026-01-05 13:39:45 -0500

    * Release note updated

--- src/bento/aboutPageData.js
+++ src/bento/aboutPageData.js
@@ -1,5 +1,5 @@
 import userGuide from '../assets/releaseNotes/C3DC User Guide-R7-V1.pdf';
-import latestRelease from'../assets/releaseNotes/C3DC_Release_Notes_R7.pdf';
+import latestRelease from'../assets/releaseNotes/C3DC_Release_Notes_R8.pdf';
 
 export const aboutData = {
   aboutHeaderText:
